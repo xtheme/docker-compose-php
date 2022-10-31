d2191b30cb774e32586751419c7f25a176dc9095
Bonn <bonn@nuts88.net>
2022-10-31 13:50:25 +0800

    feat: 初始架构
    php-fpm + nginx + mysql + redis

---
+++ php-fpm/aliases.sh
@@ -0,0 +1,8 @@
+alias ll='ls -l'
+alias ls='ls -F --color=auto --show-control-chars'
+
+alias pa="php artisan"
+alias artisan="php artisan"
+alias sail='[ -f sail ] && bash sail || bash vendor/bin/sail'
+
+alias hyperf="php bin/hyperf.php"
