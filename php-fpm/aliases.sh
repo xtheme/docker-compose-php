b58ec4a5e37f2dfb90155073ebf99dc21544e777
RB <rb.lin.hm@gmail.com>
2023-03-03 11:51:28 +0800

    refactor: 調整 PHP 環境, 加入 alias / 測試工具

--- php-fpm/aliases.sh
+++ php-fpm/aliases.sh
@@ -4,5 +4,3 @@ alias ls='ls -F --color=auto --show-control-chars'
 alias pa="php artisan"
 alias artisan="php artisan"
 alias sail='[ -f sail ] && bash sail || bash vendor/bin/sail'
-
-alias hyperf="php bin/hyperf.php"
---
+++ php-swoole/aliases.sh
@@ -0,0 +1,6 @@
+alias ll='ls -alF'
+
+alias pa="php artisan"
+alias hy="php bin/hyperf.php"
+alias octane="pa octane:start --watch --host=0.0.0.0 --port=8000 --task-workers=6 --max-requests=250"
+
