ecc1d0912b454150c3e9c4aa54c2117d201b1baa
RB <rb@terpro.com>
2025-02-05 14:34:17 +0800

    feat: 專案 nginx 配置
    這些專案都是 Laravel

--- php-fpm/aliases.sh
+++ php-fpm/aliases.sh
@@ -4,3 +4,11 @@ alias ls='ls -F --color=auto --show-control-chars'
 alias pa="php artisan"
 alias artisan="php artisan"
 alias sail='[ -f sail ] && bash sail || bash vendor/bin/sail'
+
+alias cdagent="cd /var/www/API_Agent/_release"
+alias cdcronjob="cd /var/www/RD-Studio-01/_release"
+alias cdexternal="cd /var/www/API_External/_release"
+alias cdfront="cd /var/www/API_Frontend/_release"
+alias cdinternal="cd /var/www/api-internal/_release"
+alias cdsocket="cd /var/www/WEB_CustomerServiceSocket/_release"
+
